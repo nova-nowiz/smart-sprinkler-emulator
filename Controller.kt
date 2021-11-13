d3158bace13d751c018c83f9f160f132dad5789c
David Considine <dmconsie@gmail.com>
2021-11-13 11:27:32 +0000

    Updated Controller to poll sensors and update sprinkler status

--- Controller.kt
+++ Controller.kt
@@ -1,17 +1,17 @@
 
 
 class Controller (
-    // class properties
-    newTime:Int,
-    newState: Boolean, // TRUE = Active, FALSE = Standby (placeholder)
-    newMode: Boolean//, // TRUE = Smart mode, FALSE = Manual mode (placeholder)
+        // class properties
+        newTime:Int,
+        newState: Boolean, // TRUE = Active, FALSE = Standby (placeholder)
+        newMode: Boolean//, // TRUE = Smart mode, FALSE = Manual mode (placeholder)
 ) {
     private var currentTime: Int
     private var state: Boolean
     private var mode: Boolean
     private var currentMoisture: Int
     private var isPrecipitating: Boolean
-    //private var registeredSensors - ArrayList of Sensor object refs
+    private var registeredSensors: ArrayList<SensorInterface>
     private var isSprinkling: Boolean
 
     init {
@@ -20,17 +20,39 @@ class Controller (
         mode = newMode
         currentMoisture = 0
         isPrecipitating = false
+        registeredSensors = arrayListOf<SensorInterface>()
         isSprinkling = false
     }
 
     // register one or more sensors with this controller
-    fun registerSensors(/* vararg of Sensor object refs */){
-
+    fun registerSensors(vararg sensorsToRegister:SensorInterface ){
+        sensorsToRegister.forEach { sensor ->
+            registeredSensors.add(sensor)
+            sensor.registerController(this);
+        }
     }
 
     fun update(newTime: Int)
     {
+        // check current state is 'Active' and mode is 'Smart'
+        if(state && mode) {
+            // poll registered sensors for data
+            registeredSensors.forEach { sensor ->
+                sensor.pollUpdates(currentTime)
+            }
 
+            // check moisture level and forecasted precipitation
+            if (currentMoisture <= 0 && !isPrecipitating && !isSprinkling) {
+                isSprinkling = true
+                // update 'Sprinkler'
+            }
+            if (isSprinkling && (currentMoisture >= 100 || isPrecipitating)) {
+                isSprinkling = false;
+                // update 'Sprinkler'
+            }
+        }
+        // progress time
+        currentTime++
     }
 
     fun setCurrentMoisture(newMoistureValue: Int){

c1988b288b542f4a4f1bb567f07356a09bc29a31
David Considine <DavidConsidine@users.noreply.github.com>
2021-11-15 10:26:31 +0000

    Update MoistureSensor.kt
    Co-authored-by: Narice <narice@tutanota.com>

--- MoistureSensor.kt
+++ MoistureSensor.kt
@@ -1,25 +1,21 @@
 class MoistureSensor (
         _currentTime: Int
 ){
-    var currentTime: Int = _currentTime
-    val deltaTime: Int = 1
     var currentMoisture: Int = 10
     val precipitationRate = 30
     val dryingRate = 5
     val sprinklingRate = 20
 
     // update will get precipitating and sprinklersOn flag from controller initially, until features coded
-    fun update(newTime: Int, isPrecipitating: Boolean, isSprinkling: Boolean) {
-        currentTime = newTime
-
-        if(isSprinkling){
-            currentMoisture += sprinklingRate * deltaTime
+    fun update(isPrecipitating: Boolean, isSprinkling: Boolean) {
+        if(isSprinkling) {
+            currentMoisture += sprinklingRate
         }
-        else if (isPrecipitating){
-            currentMoisture += precipitationRate * deltaTime
+        else if (isPrecipitating) {
+            currentMoisture += precipitationRate
         }
         else {
-            currentMoisture -= dryingRate * deltaTime
+            currentMoisture -= dryingRate
         }
     }
 }
